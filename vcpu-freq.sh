fe683364d752f3122fb13033bc012a2159c02123
Cloud User <ec2-user@ip-10-0-1-22.eu-west-1.compute.internal>
2023-01-26 13:31:31 +0000

    w nodename

--- vcpu-freq.sh
+++ vcpu-freq.sh
@@ -2,19 +2,20 @@
 
 METRICNAME="f"
 
-if [ -z $1 ] || [ -z $2 ]; then
-	echo "Usage: $0 <PUSHGWIP> <interval>"
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
+	echo "Usage: $0 <NODENAME> <PUSHGWIP> <interval>"
 	exit 1
 fi
 
-PUSHGWIP=$1
-INTERVAL=$2
+NODENAME=$1
+PUSHGWIP=$2
+INTERVAL=$3
 
 while [ true ]; do
 	i=0
 	for f in ` grep -i mhz /proc/cpuinfo | awk ' { print $4 } ' `; do
 		# echo "f(vCPU${i})=${f}"
-		echo "${METRICNAME} ${f}" | curl --noproxy $PUSHGWIP --connect-timeout 2 --data-binary @- "http://$PUSHGWIP:9091/metrics/job/demo/instance/vCPU${i}"
+		echo "${METRICNAME} ${f}" | curl --noproxy $PUSHGWIP --connect-timeout 2 --data-binary @- "http://$PUSHGWIP:9091/metrics/job/vCPU${i}/instance/${NODENAME}"
 		((i++))
 	done
 	echo -n "."
